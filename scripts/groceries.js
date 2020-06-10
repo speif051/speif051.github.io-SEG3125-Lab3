c8a5d9b7d1b28b665f466e72543921f73fc57abb
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-09 21:27:22 -0400

    Update groceries.js

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -1,4 +1,3 @@
-	
 // Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products		 
 
@@ -6,24 +5,81 @@ var products = [
 	{
 		name: "brocoli",
 		vegetarian: true,
-		glutenFree: true,
-		price: 1.99
+		glutenFree: false,
+		organic: true,
+		price: 1.45
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
+		organic: false,
 		price: 2.35
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
+		organic: false,
 		price: 10.00
-	}
+	},
+	{
+		name: "corn",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 2.99
+	},
+	{
+		name: "hotdog",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 4.49
+	},
+	{
+		name: "pasta",
+		vegetarian: true,
+		glutenFree: false,
+		organic: false,
+		price: 3.00
+	},
+	{
+		name: "apple",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: .50
+	},
+	{
+		name: "carrot",
+		vegetarian: true,
+		glutenFree: true,
+		organic: true,
+		price: 1.80
+	},
+	{
+		name: "eggs",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 8.00
+	},
+	{
+		name: "milk",
+		vegetarian: true,
+		glutenFree: true,
+		organic: false,
+		price: 3.20
+	},
+	{
+		name: "ham",
+		vegetarian: false,
+		glutenFree: true,
+		organic: false,
+		price: 14.00
+	},
 ];
-	
-
 
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
@@ -35,6 +91,9 @@ function restrictListProducts(prods, restriction) {
 			product_names.push(prods[i].name);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+			product_names.push(prods[i].name);
+		}
+		else if ((restriction == "Organic") && (prods[i].organic == true)){
 			product_names.push(prods[i].name);
 		}
 		else if (restriction == "None"){
