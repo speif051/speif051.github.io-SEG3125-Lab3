26b04237ac6bdd375c6cf58f60d6fa659300ab18
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-10 18:33:02 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+openInfo(evt, "Client"); // Display the current tab
+
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
 function openInfo(evt, tabName) {
