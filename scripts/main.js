6453228f93614adacf9d3c9d2747b81622cef721
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-10 19:35:19 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ function openInfo(evt, tabName) {
 	tablinks = document.getElementsByClassName("tablinks");
 	for (i = 0; i < tablinks.length; i++) {
 		tablinks[i].className = tablinks[i].className.replace(" active", "");
+		tablinks[i].style.display = "none";
 	}
 
 	// Show the current tab, and add an "active" class to the button that opened the tab
